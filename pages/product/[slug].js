5cc6150d55682e2011d3c110b30277b80fbd3689
Thomas Boittin <tboittin@gmail.com>
2021-10-07 11:24:18 +0200

    Fetch products from MongoDb

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,4 @@
 import React from "react"
-import { useRouter } from "next/router"
 import NextLink from "next/link"
 import Image from "next/image"
 
@@ -13,15 +12,15 @@ import {
   Button,
 } from "@material-ui/core"
 
-import data from "../../utils/data"
 import Layout from "../../components/Layout"
 import useStyles from "../../utils/styles"
 
-export default function ProductScreen() {
+import db from "../../utils/db"
+import Product from "../../models/Product"
+
+export default function ProductScreen(props) {
+  const {product} = props
   const classes = useStyles()
-  const router = useRouter()
-  const { slug } = router.query
-  const product = data.products.find(a => a.slug === slug)
   if (!product) {
     return <div>Product Not Found</div>
   }
@@ -101,4 +100,18 @@ export default function ProductScreen() {
       </Grid>
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  const {params} = context
+  const {slug} = params
+
+  await db.connect()
+  const product = await Product.findOne({slug}).lean()
+  await db.disconnect()
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    }
+  }
 }
