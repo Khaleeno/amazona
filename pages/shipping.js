5cba1c1e5854a9365f35b7eccb0853375d4556cb
Thomas Boittin <tboittin@gmail.com>
2021-10-11 16:12:07 +0200

    fix shipping deploment issue

--- pages/shipping.js
+++ pages/shipping.js
@@ -5,7 +5,7 @@ import { Store } from '../utils/store'
 
 export default function Shipping() {
   const router = useRouter()
-  const { state, dispatch } = useContext(Store)
+  const { state } = useContext(Store)
   const { userInfo } = state
 
   if (!userInfo) {
