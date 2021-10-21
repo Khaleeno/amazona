c101ebcc4d4e87cb95261ae5c5fb458c1a6834fe
Thomas Boittin <tboittin@gmail.com>
2021-10-21 16:30:35 +0200

    lesson 31 up to 17:35

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -25,7 +25,9 @@ import Layout from "../../components/Layout"
 import { Store } from "../../utils/store"
 import useStyles from "../../utils/styles"
 import CheckoutWizard from "../../components/CheckoutWizard"
-import { getError } from "../../utils/error"
+import { getError, onError } from "../../utils/error"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
+import { useSnackbar } from "notistack"
 
 function reducer(state, action) {
   switch (action.type) {
@@ -42,6 +44,7 @@ function reducer(state, action) {
 
 function Order({ params }) {
   const orderId = params.id
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer()
   const classes = useStyles()
   const router = useRouter()
   const { state } = useContext(Store)
@@ -64,7 +67,7 @@ function Order({ params }) {
     isPaid,
     paidAt,
     isDelivered,
-    deliveredAt
+    deliveredAt,
   } = order
 
   useEffect(() => {
@@ -84,8 +87,59 @@ function Order({ params }) {
     }
     if (!order._id || (order._id && order._id !== orderId)) {
       fetchOrder()
+    } else {
+      const loadPaypalScript = async () => {
+        const { data: clientId } = await axios.get("/api/keys/paypal", {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        })
+        paypalDispatch({
+          type: "resetOptions",
+          value: {
+            "client-id": clientId,
+            currency: "USD",
+          },
+        })
+        paypalDispatch({ type: "setLoadingStatus", value: "pending" })
+      }
+      loadPaypalScript()
     }
   }, [order])
+
+  const { closeSnackbar, enqueueSnackbar } = useSnackbar()
+
+  const createOrder = (data, actions) => {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: { value: totalPrice },
+          },
+        ],
+      })
+      .then(orderId => {
+        return orderId
+      })
+  }
+
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        dispatch({ type: "PAY_REQUEST" })
+        const { data } = await axios.put(
+          `/api/orders/${order._id}/pay`,
+          details,
+          {
+            headers: { authorization: `Bearer ${userInfo.token}` },
+          }
+        )
+        dispatch({ type: "PAY_SUCCESS", payload: data })
+        enqueueSnackbar("Order is paid", { variant: "success" })
+      } catch (err) {
+        dispatch({ type: "PAY_FAIL", payload: getError(err) })
+        enqueueSnackbar(getError(err), { variant: "error" })
+      }
+    })
+  }
 
   return (
     <Layout title={`Order ${orderId}`}>
@@ -128,12 +182,9 @@ function Order({ params }) {
                   </Typography>
                 </ListItem>
                 <ListItem>{paymentMethod}</ListItem>
-                
+
                 <ListItem>
-                  Status:{" "}
-                  {isPaid
-                    ? `paid at ${paidAt}`
-                    : "not paid"}
+                  Status: {isPaid ? `paid at ${paidAt}` : "not paid"}
                 </ListItem>
               </List>
             </Card>
@@ -242,6 +293,21 @@ function Order({ params }) {
                     </Grid>
                   </Grid>
                 </ListItem>
+                {!isPaid && (
+                  <ListItem>
+                    {isPending ? (
+                      <CircularProgress />
+                    ) : (
+                      <div className={classes.fullWidth}>
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
+                        ></PayPalButtons>
+                      </div>
+                    )}
+                  </ListItem>
+                )}
               </List>
             </Card>
           </Grid>
